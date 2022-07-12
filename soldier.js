4918f9ce6f41a86e8c61564927bfccc21fa612be
GooA-NA <nasirgudesev@gmail.com>
2022-07-12 15:21:48 +0300

    Добавил для функций логику

--- soldier.js
+++ soldier.js
@@ -8,11 +8,29 @@ let soldier = {
     supplies: 3,
 
     toShot() {
-       
+        if (this.weapon.numberOfRounds != 0) {
+            this.weapon.numberOfRounds--;
+            console.log("Бах-Бах")
+        } else {
+            console.log("Обойма пуста. Перезарядитесь")
+        }
     },
     toRecharge() {
-       
+        if (this.supplies > 0) {
+            this.weapon.numberOfRounds = 30;
+            this.supplies--;
+            console.log("Перезарядка")
+        }
+        else {
+            console.log("Не осталось препасов")
+        }
     },
     toHurt() {
+        if (this.helth > 0) {
+            this.helth--;
+        }
+        else {
+            console.log("Ты проиграл")
+        }
     },
 };
