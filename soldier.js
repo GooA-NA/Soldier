0235cf78283f00d430a40c476b8ac08039c66c51
GooA-NA <nasirgudesev@gmail.com>
2022-07-12 15:20:44 +0300

    Создал обЪект soldier, дал значения для ключей

---
+++ soldier.js
@@ -0,0 +1,18 @@
+let soldier = {
+    name: "Jane",
+    helth: 10,
+    weapon: {
+        nameWeapon: "M1911",
+        numberOfRounds: 30
+    },
+    supplies: 3,
+
+    toShot() {
+       
+    },
+    toRecharge() {
+       
+    },
+    toHurt() {
+    },
+};
